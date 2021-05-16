5de04bc9c7330f9848a1b4f1b2060688f234a69c
Ammlakh <mrinal.lakhotia@gmail.com>
2021-05-16 10:39:15 -0700

    conf

--- R/pluralize_gift.R
+++ R/pluralize_gift.R
@@ -12,16 +12,12 @@
 #'
 #' @export
 pluralize_gift <- function(gift){
-  gift <- gift %>%
   if(str_detect(gift, "y")){
     gift <- str_replace(gift, "y", "ies")
   } else if(str_detect(gift, "oo")){
     gift <- str_replace(gift, "oo", "ee")
   } else{
     gift <- paste0(gift, "s")
-
   }
-
-
 }
 
